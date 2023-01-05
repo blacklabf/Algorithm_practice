12d996a68f1dddbc375336ff68540bea3cc7a508
blacklabf <bjo8244@naver.com>
2023-01-05 10:44:39 +0900

    백 문자열 반복

--- 백준/B2675.py
+++ 백준/B2675.py
@@ -2,15 +2,24 @@
 # 입력
 # 1) 테스트 케이스 T 2) 반복횟수 R 과 문자열 S
 
+# T = int(input())
+# for t in range(T):
+#     R, S = input().split(' ') # R, S 모두 string 형으로 출력
+#     P = ''
+#     for A in S: #이때 S가 abc 이면 A는 순차적으로 a, b, c
+#         P += int(R) * A # P를 위에서 먼저 정의해줘야 함.
+#     print(P)
+        
 T = int(input())
 for t in range(T):
-    R, S = input().split(' ') # R, S 모두 string 형으로 출력
-    P = ''
-    for A in S: #이때 S가 abc 이면 A는 순차적으로 a, b, c
-        P += int(R) * A # P를 위에서 먼저 정의해줘야 함.
-    print(P)
-        
-
+    r , s = input().split()
+    a = len(s)
+    A = []
+    for i in range(a):
+        B = s[i] * int(r)
+        A.append(B)
+    print(''.join(A))
+    
 
 
 
