2eefaae2e206585455cbb5f9d164660e453a3492
blacklabf <bjo8244@naver.com>
2023-01-08 10:20:18 +0900

    백준 소수구하기 for else문

--- 백준/수학/B1929.py
+++ 백준/수학/B1929.py
@@ -26,14 +26,14 @@
 #         j += 1
 # print(*prime, sep='\n')
 
-# 풀이3: 에라토스테네스의 체 ( 다른풀이)
+# 풀이3: 에라토스테네스의 체 ( 다른풀이 : for else 문)
 m,n=map(int,input().split())
 
 for i in range(m,n+1):
-    if i==1:#1은 소수가 아니므로 제외
+    if i==1: #1 제외
         continue
     for j in range(2,int(i**0.5)+1):
-        if i%j==0: #약수가 존재하므로 소수가 아님
-            break   #더이상 검사할 필요가 없으므로 멈춤
-    else:
+        if i%j==0: # 약수 존재 -> 소수 아님
+            break  
+    else: # for -else 문
         print(i)
