7ee3438c0a481d3647fa82ce8c85f202e115b1c9
soyoung <bjo8244@naver.com>
2023-01-08 21:27:22 +0900

    백준 소인수분해

--- 백준/수학/B11653.py
+++ 백준/수학/B11653.py
@@ -1,15 +1,3 @@
-# N = int(input())
-# a = []
-# if N == 1:
-#     pass
-# else:
-#     for i in range(2,N+1):
-#         if N % i != 0:
-#             continue
-#         else:
-#             print(i)
-#             N = N // i
-
 N = int(input())
 i = 2
 
@@ -21,6 +9,3 @@ while N != 1:
         print(i)
         N = N//i
         i = 2
-
-
-
