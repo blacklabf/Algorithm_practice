9fe61d06e036551e23f9dbb8e9813dfefa23cf58
blacklabf <bjo8244@naver.com>
2023-01-08 11:04:31 +0900

    백준 소수찾기 for else continue break 이용

--- 백준/수학/B1978.py
+++ 백준/수학/B1978.py
@@ -1,15 +1,12 @@
-# # N 개 중에서 소수가 몇 개인지 찾기
-# N = int(input())
-# for i in range(N):
-#     A = input().split()
-# print(A)
-
-a = []    # 빈 리스트 생성
- 
-for i in range(3):
-    line = []              # 안쪽 리스트로 사용할 빈 리스트 생성
-    for j in range(2):
-        line.append(0)     # 안쪽 리스트에 0 추가
-    a.append(line)         # 전체 리스트에 안쪽 리스트를 추가
- 
-print(a)
+N = int(input())
+prime = list(map(int, input().split()))
+prime_new = []
+for i in range(N):
+    if prime[i] == 1:
+        continue
+    for j in range(2, prime[i]):
+        if prime[i] % j == 0:
+            break
+    else: 
+        prime_new.append(prime[i])
+print(len(prime_new))
