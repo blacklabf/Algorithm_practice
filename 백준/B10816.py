6b3699955b52451b17b2d349e5c97c068c5d1ad3
Bang Soyoung <bjo8244@naver.com>
2023-01-24 19:00:30 +0900

    백준 숫자카드2 이분탐색풀이

--- 백준/B10816.py
+++ 백준/B10816.py
@@ -1,18 +1,37 @@
 # 백준 숫자 카드 2
-# 수정한 풀이 -> 이것도 ans가 0으로만 나옴
+# 이분탐색으로 풀기
+
+# 이진 탐색 소스코드 구현 (재귀 함수)
+def binary_search(array, target, start, end):
+    if start > end:
+        return 0
+    mid = (start + end) // 2
+    # 찾은 경우 중간점 인덱스 반환
+    if array[mid] == target:
+        return count.get(target)
+    # 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid - 1)
+    # 중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
+    else:
+        return binary_search(array, target, mid + 1, end)
+
+
 import sys ; input = sys.stdin.readline
 n = int(input().strip())
-nList = list(map(int, input().strip().split()))
+array = sorted(list(map(int, input().strip().split())))
 m = int(input().strip())
-mList = list(map(int, input().strip().split()))
+target = list(map(int, input().strip().split()))
 
-
-
-ans =[0]* m
-for i in range(m):
-    if mList[i] in nList:
-        ans[i] = nList.count(mList[i])
+count = {}
+for i in array:
+    if i in count:
+        count[i] += 1
     else:
-        pass
+        count[i] = 1
 
-print(*ans)
+for i in target:
+    print(binary_search(array, i, 0, len(array)-1), end=' ')
+
+
+
