093882f90d60b6b30ff19a37b73539cc43a58512
Bang Soyoung <bjo8244@naver.com>
2023-01-24 16:01:40 +0900

    백준 정렬

--- 백준/B10814.py
+++ 백준/B10814.py
@@ -1,7 +1,7 @@
 # 백준 나이순정렬
-# 틀린 풀이
+# 틀린 풀이 2 : 나이를 int형으로 받고 출력해야 함.
 n = int(input())
 nList = [input().split() for _ in range(n)]
-mList = sorted(nList, key = lambda x : x[0])
+nList.sort(key = lambda x : x[0])
 for i in range(n):
-    print(mList[i][0], mList[i][1])
+    print(int(nList[i][0]), nList[i][1])
