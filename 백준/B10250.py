ab43a5ecd9f8c498482eccdf3196106a5fbc6438
Bang Soyoung <bjo8244@naver.com>
2023-01-24 14:32:55 +0900

    백준 ACM호텔

--- 백준/B10250.py
+++ 백준/B10250.py
@@ -1,12 +1,14 @@
-# 틀린 풀이
+# 백준 ACM 호텔
+# 수정한 풀이 (str -> int)
+# 나누어 떨어지는 경우에 대해서 x,y 모두 차분하게 생각하기
 import sys ; input = sys.stdin.readline
-test = int(input())
+test = int(input().strip())
 for i in range(test):
     a, b, c = map(int, input().strip().split())
-    x = str(c % a) 
-    y = str((c // a)+ 1)
-    if len(y) == 1:
-        y = '0' + y
-    else : 
-        pass
-    print (x + y)
+    if c % a != 0:
+        x = c % a 
+        y = (c // a)+ 1
+        print (x* 100 + y)
+    else: 
+        y = c // a
+        print (a* 100 + y)
