2039a828bc29bdab135136ef1e033e959c1e3844
Bang Soyoung <bjo8244@naver.com>
2023-01-31 17:39:47 +0900

    백준 큰 수

--- 백준/수학/B7894.py
+++ 백준/수학/B7894.py
@@ -1,5 +1,6 @@
 # 백준 7894 큰 수
-# factorial , log , trunc라이브러리 사용
+# 라이브러리 사용
+# factorial을 구해주면 값이 커져서 시간초과 -> log 곱셈 성질 이용
 
 import sys; input = sys.stdin.readline
 import math
@@ -7,6 +8,7 @@ t = int(input())
 
 for i in range(t):
     a = int(input())
-    b = math.factorial(a)
-    c = math.log10(b)
-    print(math.trunc(c) +1 )
+    s = 0
+    for j in range(1, a+1):
+        s += math.log10(j)
+    print(math.trunc(s) +1)
