70cbcefca72eaad8b2ee8b199307e89c8eca94cb
Bang Soyoung <bjo8244@naver.com>
2023-01-31 17:23:39 +0900

    백준 큰 수 너무 깊은 재귀

--- 백준/수학/B7894.py
+++ 백준/수학/B7894.py
@@ -1,11 +1,15 @@
 # 백준 7894 큰 수
-# 시간초과
+# 재귀 -> 백준에서는 1000번까지 가능인데 이 문제는 10^7만큼 해야해서 런타임에러
+
 import sys; input = sys.stdin.readline
 t = int(input())
 
+def factori(a):
+    if a > 1:
+        return a * factori(a-1)
+    else:
+        return 1
+
 for i in range(t):
     a = int(input())
-    b = 1
-    for j in range(1, a+1):
-        b *= j
-    print(len(str(b)))
+    print(len(str(factori(a))))
