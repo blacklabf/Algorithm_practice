49c8a03d988eadb113465cfb05223f9ffba5fae6
Bang Soyoung <bjo8244@naver.com>
2023-01-31 17:26:07 +0900

    백준 시간초과2

--- 백준/수학/B7894.py
+++ 백준/수학/B7894.py
@@ -1,15 +1,11 @@
 # 백준 7894 큰 수
-# 재귀 -> 백준에서는 1000번까지 가능인데 이 문제는 10^7만큼 해야해서 런타임에러
+# 라이브러리 사용
 
 import sys; input = sys.stdin.readline
+import math
 t = int(input())
-
-def factori(a):
-    if a > 1:
-        return a * factori(a-1)
-    else:
-        return 1
 
 for i in range(t):
     a = int(input())
-    print(len(str(factori(a))))
+    b = math.factorial(a)
+    print(len(str(b)))
